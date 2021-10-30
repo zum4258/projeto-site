2d9c6ca73c6c7c069074fb0c15a5cadadea50296
muniz19 <munizvinicius861@gmail.com>
2021-10-29 21:23:13 -0300

    cadastrando nome

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD - Cadastrar</title>
+</head>
+<body>
+    <h1>Cadastrar Usuario</h1>
+    <form method="POST" action="processa.php">
+     <label>Nome: </label>
+     <input type="text" name="nome" placeholder="Digite seu nome completo"><br><br>
+
+    </form>
+</body>
+</html>
